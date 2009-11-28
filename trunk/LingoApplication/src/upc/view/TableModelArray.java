bc313b9835a09343f1120b8b23e51467c1d33316
zaxa85 <zaxa85@2deedf22-bda3-11de-b286-51bc83d83949>
2009-11-28 02:41:55 +0000

    Configurations settings

--- trunk/LingoApplication/src/upc/view/TableModelArray.java
+++ trunk/LingoApplication/src/upc/view/TableModelArray.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package upc.view;
 
 import javax.swing.event.EventListenerList;
@@ -89,6 +85,4 @@ public class TableModelArray extends AbstractTableModel {
     public void setListenerList(EventListenerList listenerList) {
         this.listenerList = listenerList;
     }
-    
-
 }

8d0992746fba8fe1a43a8209e4dac58a5bb4f70c
zaxa85 <zaxa85@2deedf22-bda3-11de-b286-51bc83d83949>
2009-12-01 17:18:50 +0000

    code advance

--- trunk/LingoApplication/src/upc/control/ControlData.java
+++ trunk/LingoApplication/src/upc/control/ControlData.java
@@ -67,6 +67,22 @@ public class ControlData {
         return result;
     }
 
+    public Vector<Object> getUnidadTestArray() {
+        Vector<Unidad> data = getUnidadTest();
+        Vector result = new Vector();
+        for (int i = 0; i < data.size(); i++) {
+            Vector<Object> temp = new Vector<Object>();
+            temp.add((data.get(i)).getNombreUnidad());
+            temp.add((data.get(i)).getAbrevUnidad());
+            temp.add((data.get(i)).getInputEmpleado());
+            temp.add((data.get(i)).getInputMercados());
+            temp.add((data.get(i)).getOutputVentasPersonas());
+            temp.add((data.get(i)).getOutputVentasNegocios());
+            result.add(temp);
+        }
+        return result;
+    }
+
     public boolean guardarUnidad(Vector<Unidad> lstUnidad) {
         Connection conn = null;
         Statement stm = null;
@@ -95,6 +111,7 @@ public class ControlData {
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
+            return false;
 
         } finally {
             try {
@@ -267,5 +284,129 @@ public class ControlData {
             }
         }
 
+    }
+
+    public Vector getUnidadTest() {
+        Vector<Unidad> lstUnidad = null;
+        Connection conn = null;
+        Statement stm = null;
+        ResultSet rs = null;
+
+        try {
+            conn = AccessJDBCUtil.getAccessDBConnection();
+            stm = conn.createStatement();
+            rs = stm.executeQuery("select nombreUnidad, abrevUnidad, data from test");
+            lstUnidad = new Vector<Unidad>();
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombreUnidad");
+                String abrev = rs.getString("abrevUnidad");
+                Double inputEmpleado = rs.getDouble("data");
+                rs.next();
+                Double inputMercados = rs.getDouble("data");
+                rs.next();
+                Double outputVentasPersonas = rs.getDouble("data");
+                rs.next();
+                Double outputVentasNegocios = rs.getDouble("data");
+                lstUnidad.add(new Unidad(nombre, abrev, inputEmpleado, inputMercados, outputVentasPersonas, outputVentasNegocios));
+            }
+
+            return lstUnidad;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            try {
+                stm.close();
+                conn.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public boolean guardarUnidadTest(Vector<Unidad> lstUnidad) {
+        Connection conn = null;
+        Statement stm = null;
+        try {
+            for (int i = 0; i < lstUnidad.size(); i++) {
+                Unidad beanUnidad = lstUnidad.get(i);
+                conn = AccessJDBCUtil.getAccessDBConnection();
+                stm = conn.createStatement();
+                StringBuilder builder = new StringBuilder();
+                builder.append("insert into Test(nombreUnidad, abrevUnidad, data) values ('");
+                builder.append(beanUnidad.getNombreUnidad());
+                builder.append("','");
+                builder.append(beanUnidad.getAbrevUnidad());
+                builder.append("',");
+                builder.append(beanUnidad.getInputEmpleado());
+                builder.append(");");
+                stm.executeUpdate(builder.toString());
+
+                builder = new StringBuilder();
+                builder.append("insert into Test(nombreUnidad, abrevUnidad, data) values ('");
+                builder.append(beanUnidad.getNombreUnidad());
+                builder.append("','");
+                builder.append(beanUnidad.getAbrevUnidad());
+                builder.append("',");
+                builder.append(beanUnidad.getInputMercados());
+                builder.append(");");
+                stm.executeUpdate(builder.toString());
+
+                builder = new StringBuilder();
+                builder.append("insert into Test(nombreUnidad, abrevUnidad, data) values ('");
+                builder.append(beanUnidad.getNombreUnidad());
+                builder.append("','");
+                builder.append(beanUnidad.getAbrevUnidad());
+                builder.append("',");
+                builder.append(beanUnidad.getOutputVentasPersonas());
+                builder.append(");");
+                stm.executeUpdate(builder.toString());
+
+                builder = new StringBuilder();
+                builder.append("insert into Test(nombreUnidad, abrevUnidad, data) values ('");
+                builder.append(beanUnidad.getNombreUnidad());
+                builder.append("','");
+                builder.append(beanUnidad.getAbrevUnidad());
+                builder.append("',");
+                builder.append(beanUnidad.getOutputVentasNegocios());
+                builder.append(");");
+                stm.executeUpdate(builder.toString());
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+
+        } finally {
+            try {
+                stm.close();
+                conn.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    public boolean limpiarUnidadTest() {
+        Connection conn = null;
+        Statement stm = null;
+        try {
+            conn = AccessJDBCUtil.getAccessDBConnection();
+            stm = conn.createStatement();
+            StringBuilder builder = new StringBuilder();
+            builder.append("delete from test");
+            return stm.executeUpdate(builder.toString()) > 0;
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        } finally {
+            try {
+                stm.close();
+                conn.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
